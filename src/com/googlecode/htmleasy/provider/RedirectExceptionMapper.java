8ce74f026c2e11e029f104819cc19aa999c56516
lhoriman <lhoriman@8149a128-91a9-11de-8ad7-8b2c2f1e2016>
2009-08-25 20:44:25 +0000

    Importing htmleasy work so far.

---
+++ src/com/googlecode/htmleasy/provider/RedirectExceptionMapper.java
@@ -0,0 +1,31 @@
+package com.googlecode.htmleasy.provider;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.googlecode.htmleasy.RedirectException;
+
+/**
+ * This mapper allows us to use RedirectException to issue, uh, redirects.
+ * 
+ * @author Jeff Schnitzer <jeff@infohazard.org>
+ */
+@Provider
+public class RedirectExceptionMapper implements ExceptionMapper<RedirectException>
+{
+	@SuppressWarnings("unused")
+	private static final Logger log = Logger.getLogger(RedirectExceptionMapper.class.getName());
+
+	/* (non-Javadoc)
+	 * @see javax.ws.rs.ext.ExceptionMapper#toResponse(java.lang.Throwable)
+	 */
+	@Override
+	public Response toResponse(RedirectException ex)
+	{
+		return Response.seeOther(ex.getPath()).build();
+	}
+
+}
