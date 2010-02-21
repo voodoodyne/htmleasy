20fcb02b6d247f236650a2375e15e07fb083e298
lhoriman <lhoriman@8149a128-91a9-11de-8ad7-8b2c2f1e2016>
2010-02-21 22:45:58 +0000

    Built against jdk 1.6, enabled the @Override annotations. Manually added jaxrs-api to eclipse build. Maven is a huge pile of shit.

--- trunk/src/main/java/com/googlecode/htmleasy/provider/HtmlWriter.java
+++ trunk/src/main/java/com/googlecode/htmleasy/provider/HtmlWriter.java
@@ -30,7 +30,7 @@ import com.googlecode.htmleasy.Viewable;
 @SuppressWarnings("unchecked")
 @Provider
 @Produces("text/html")
-public class HtmlWriter implements MessageBodyWriter
+public class HtmlWriter implements MessageBodyWriter<Object>
 {
 	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(HtmlWriter.class.getName());
@@ -38,7 +38,7 @@ public class HtmlWriter implements MessageBodyWriter
 	/* (non-Javadoc)
 	 * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
 	 */
-	//@Override
+	@Override
 	public long getSize(Object obj, Class type, Type genericType, Annotation[] annotations, MediaType mediaType)
 	{
 		// No chance of figuring this out ahead of time
@@ -48,7 +48,7 @@ public class HtmlWriter implements MessageBodyWriter
 	/* (non-Javadoc)
 	 * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
 	 */
-	//@Override
+	@Override
 	public boolean isWriteable(Class type, Type genericType, Annotation[] annotations, MediaType mediaType)
 	{
 		if (Viewable.class.isAssignableFrom(type))
@@ -60,7 +60,7 @@ public class HtmlWriter implements MessageBodyWriter
 	/* (non-Javadoc)
 	 * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
 	 */
-	//@Override
+	@Override
 	public void writeTo(Object obj, Class type, Type genericType, Annotation[] annotations, MediaType mediaType,
 			MultivaluedMap httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException
 	{
