4345baded4f7ca4cb4a2ecb71a432c90623a43f2
viviansteller@gmail.com <viviansteller@gmail.com@8149a128-91a9-11de-8ad7-8b2c2f1e2016>
2011-06-15 12:58:16 +0000

    externalized view resolving into separate class

--- trunk/src/main/java/com/googlecode/htmleasy/provider/ViewWriter.java
+++ trunk/src/main/java/com/googlecode/htmleasy/provider/ViewWriter.java
@@ -18,7 +18,6 @@ import javax.ws.rs.ext.Provider;
 import org.jboss.resteasy.spi.InternalServerErrorException;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
-import com.googlecode.htmleasy.View;
 import com.googlecode.htmleasy.ViewWith;
 import com.googlecode.htmleasy.Viewable;
 
@@ -34,10 +33,13 @@ public class ViewWriter implements MessageBodyWriter<Object>
 	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(ViewWriter.class.getName());
 	
+	private ViewResolver viewResolver = new ViewResolver();
+	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
+	 * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object, java.lang.Class, java.lang.reflect.Type,
+	 * java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
 	 */
 	@Override
 	public long getSize(Object obj, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
@@ -49,42 +51,30 @@ public class ViewWriter implements MessageBodyWriter<Object>
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
+	 * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class, java.lang.reflect.Type,
+	 * java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
 	 */
 	@Override
 	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
 	{
-		if (Viewable.class.isAssignableFrom(type))
-			return true;
-		else
-			return this.getViewWith(type, genericType, annotations) != null;
+		return viewResolver.isResolvable(type, genericType, annotations);
 	}
 	
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType,
-	 * javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
+	 * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object, java.lang.Class, java.lang.reflect.Type,
+	 * java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
+	 * java.io.OutputStream)
 	 */
 	@Override
 	public void writeTo(Object obj, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders,
 			OutputStream entityStream) throws IOException, WebApplicationException
 	{
-		Viewable viewingPleasure;
+		Viewable viewingPleasure = viewResolver.getView(obj, type, genericType, annotations);
 		
-		if (obj instanceof Viewable)
-		{
-			viewingPleasure = (Viewable) obj;
-		}
-		else
-		{
-			// Since isWriteable should have been called, this should never be null
-			ViewWith viewWith = this.getViewWith(type, genericType, annotations);
-			if (viewWith == null)
-				throw new InternalServerErrorException("No " + ViewWith.class.getSimpleName() + " annotation found for object of type " + type.getName());
-			
-			viewingPleasure = new View(viewWith.value(), obj, viewWith.modelName());
-		}
+		if (viewingPleasure == null)
+			throw new InternalServerErrorException("No " + ViewWith.class.getSimpleName() + " annotation found for object of type " + type.getName());
 		
 		HttpServletRequest request = ResteasyProviderFactory.getContextData(HttpServletRequest.class);
 		HttpServletResponse response = ResteasyProviderFactory.getContextData(HttpServletResponse.class);
@@ -99,39 +89,14 @@ public class ViewWriter implements MessageBodyWriter<Object>
 		}
 	}
 	
-	/**
-	 * @param genericType
-	 *            TODO
-	 * @return the relevant view annotation, or null if no view can be determined
-	 */
-	protected ViewWith getViewWith(Class<?> type, Type genericType, Annotation[] methodAnnotations)
+	public ViewResolver getViewResolver()
 	{
-		if (methodAnnotations != null)
-		{
-			for (Annotation anno : methodAnnotations)
-			{
-				if (anno instanceof ViewWith)
-				{
-					Class<?> forClass = ((ViewWith) anno).ifClass();
-					// Note that View.class is a sentinel value indicating "all classes"
-					if (ViewWith.class.equals(forClass) || forClass.isAssignableFrom(type))
-					{
-						return (ViewWith) anno;
-					}
-				}
-			}
-		}
-		
-		if (type.isAnnotationPresent(ViewWith.class))
-		{
-			return (ViewWith) type.getAnnotation(ViewWith.class);
-		}
-		
-		if (genericType instanceof Class && ((Class<?>) genericType).isAnnotationPresent(ViewWith.class))
-		{
-			return (ViewWith) ((Class<?>) genericType).getAnnotation(ViewWith.class);
-		}
-		
-		return null;
+		return viewResolver;
 	}
+	
+	public void setViewResolver(ViewResolver viewResolver)
+	{
+		this.viewResolver = viewResolver;
+	}
+	
 }
